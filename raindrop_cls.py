71296981525a36057c58e478055bde7ba6c48647
Marc Wiedermann <marcwiedermann@posteo.de>
2016-07-03 22:45:01 +0200

    BUGFIX: pattern was not stopping

--- raindrop_cls.py
+++ raindrop_cls.py
@@ -31,7 +31,7 @@ class RaindropPattern(threading.Thread):
         is_drop = np.zeros((self.n_rows, self.n_led)) 
 
         n_drops = 0 
-        while not ((n_drops == 100) and (brightness == 0).all()):
+        while (not ((n_drops == 100) and (brightness == 0).all()) and (self.stopping == False)):
             where_drop = np.where(is_drop)
 
             brightness[where_drop] = current_color
