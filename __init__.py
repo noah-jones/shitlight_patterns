1e906d2ad4418e424fce3807c7938a8bc4c85473
Lorenz Drescher <dev@tmtnslt.com>
2016-07-03 23:41:38 +0200

    Added Circles, Klapp and Raindrop

--- __init__.py
+++ __init__.py
@@ -1,12 +1,28 @@
 
 
 from cross_cls import CrossPattern
+
 from shooting_star_cls import ShootingStarPattern
+
 from unicolor_cls import UnicolorPattern
+
+from raindrop_cls import RaindropPattern
+
+from circle_cls import CirclePattern
+from circle_cls import CirclePatternInv
+
+from klapp_cls import KlappPatternFast
+from klapp_cls import KlappPatternSlow
 
 
 patterns = [ ('Cross', CrossPattern),
              ('Shooting Star', ShootingStarPattern),
-             ('Unicolor', UnicolorPattern) ]
+             ('Unicolor', UnicolorPattern),
+             ('Raindrop', RaindropPattern),
+             ('Klapp (fast)', KlappPatternFast),
+             ('Klapp (slow)', KlappPatternSlow),
+             ('Circles (in2out)', CirclePattern),
+             ('Circles (out2in)', CirclePatternInv) ]
+
 
 
