a0d8b2252d0bff58bd65098a259fe7537be92f17
Marc Wiedermann <marcwiedermann@posteo.de>
2016-07-04 23:31:53 +0200

    add propeller

--- __init__.py
+++ __init__.py
@@ -1,20 +1,21 @@
 
 
 from cross_cls import CrossPattern
-
 from shooting_star_cls import ShootingStarPattern
-
 from unicolor_cls import UnicolorPattern
 from raindrop_cls import RaindropPattern
 from klapp_cls import KlappPatternSlow
 from klapp_cls import KlappPatternFast
 from mr_clean_cls import MrCleanPattern, MrCleanFast, MrCleanSuperfast
 from wave_cls import WavePatternSlow, WavePatternFast
+from propeller_cls import PropellerPattern, PropellerPatternSlow
 
 patterns = [ ('Cross', CrossPattern),
              ('Shooting Star', ShootingStarPattern),
              ('Unicolor', UnicolorPattern),
              ('Raindrops', RaindropPattern),
+             ('Propeller - Fast', PropellerPattern),
+             ('Propeller - Slow', PropellerPatternSlow),
              ('Klapp - Fast',  KlappPatternFast),
              ('Klapp - Slow', KlappPatternSlow),
              ('Wave - Fast',  WavePatternFast),
---
+++ propeller_cls.py
@@ -0,0 +1,45 @@
+import shytlight
+import time
+import cv2
+import numpy as np
+import threading
+
+class PropellerPattern(threading.Thread):
+    def __init__(self):
+        super(PropellerPattern, self).__init__()
+
+        self.stopping = False
+
+        self.base_color = (0x44, 0x64, 0x55) # stromboli
+        self.base_color = (0x0, 0x0, 0x0)
+        self.highlight_color = (0xfd, 0xd2, 0x62) # cream can
+        
+        self.bpm = 124.
+
+    def stop(self):
+	self.stopping = True
+
+    def run(self):
+        while(self.stopping == False):
+            self.tlp = int(1/(self.bpm/4.)*60./100.*122.)
+            for j in range(100):
+                # create image on large basis
+                circles = np.zeros((50,80, 3), np.uint8)
+                circles[:,:]= np.flipud(self.base_color) # numpy ordering is reversed, BGR
+                # draw a line
+                endpoint = ((np.sin(np.pi*j/50)*100+40).astype(np.int),(np.cos(np.pi*j/50)*100+25).astype(np.int))
+                inv_endpoint = ((np.sin(np.pi*j/50)*-100+40).astype(np.int),(np.cos(np.pi*j/50)*-100+25).astype(np.int))
+                cv2.line(circles, (40,25), (endpoint), self.highlight_color, 8)
+                cv2.line(circles, (40,25), (inv_endpoint), self.highlight_color, 8)
+                # blur circle
+                bl_circles = cv2.blur(circles, (11,11))
+                # resize image to our real led size
+                test_geometry = cv2.resize(bl_circles,(8,5))[::,:,:]
+                # add frame to buffer
+                shytlight.add_frame(2, test_geometry)
+
+class PropellerPatternSlow(PropellerPattern):
+    def __init__(self):
+        super(PropellerPatternSlow, self).__init__()
+        self.bpm = 32.
+
