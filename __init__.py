af8b20cc8034c4789a19bd4c5320437894efe69e
Marc Wiedermann <marcwiedermann@posteo.de>
2016-07-04 23:12:36 +0200

    whatever

--- __init__.py
+++ __init__.py
@@ -3,10 +3,21 @@
 from cross_cls import CrossPattern
 from shooting_star_cls import ShootingStarPattern
 from unicolor_cls import UnicolorPattern
-
+from raindrop_cls import RaindropPattern
+from klapp_cls import KlappPatternSlow
+from klapp_cls import KlappPatternFast
+from mr_clean_cls import MrCleanPattern, MrCleanFast, MrCleanSuperfast
+from wave_cls import WavePatternSlow, WavePatternFast
 
 patterns = [ ('Cross', CrossPattern),
              ('Shooting Star', ShootingStarPattern),
-             ('Unicolor', UnicolorPattern) ]
-
-
+             ('Unicolor', UnicolorPattern),
+             ('Raindrops', RaindropPattern),
+             ('Klapp - Fast',  KlappPatternFast),
+             ('Klapp - Slow', KlappPatternSlow),
+             ('Wave - Fast',  WavePatternFast),
+             ('Wave - Slow', WavePatternSlow),
+             ('MR. Clean classic', MrCleanPattern),
+             ('MR. Clean on drugs', MrCleanFast),
+             ('MR. Clean on speed', MrCleanSuperfast),
+             ]
